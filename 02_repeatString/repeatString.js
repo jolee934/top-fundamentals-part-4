1f2917fbbab794b99e6b4b3724fabd7c26fbc3f1
hosh <jolee934@gmail.com>
2022-08-18 17:45:29 -0700

    repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    if (n < 0) return "ERROR";
+   
+    let string = ''
+    for (let i= 0; i<n ; i++) {
+        string+= word
+    }
+    return string;
+    
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
