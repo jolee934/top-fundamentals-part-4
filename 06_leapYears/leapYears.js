acab692aeb090a0f35a33a42fc53519fc3630185
hosh <jolee934@gmail.com>
2022-08-19 15:06:52 -0700

    leapYears.spec.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%4 == 0) {
+        if (year%100 == 0) {
+            if (year%400 == 0) {
+                return true;
+            } return false;
+        } return true;
+    }
+    return false;
 };
 
+/*
+1. divisble by 4
+2.  divisible by 100 is not, unless divisible by 400
+*/
 // Do not edit below this line
 module.exports = leapYears;
